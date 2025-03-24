473a5c554cf2417d5fc2c3534d2c4cae2eeac127
zacklim33 <limllz13@gmail.com>
2025-03-24 17:45:48 +0800

    added login API authentication

--- secondChance-backend/routes/authRoutes.js
+++ secondChance-backend/routes/authRoutes.js
@@ -56,4 +56,64 @@ router.post('/register', async (req, res) => {
     }
 });
 
+
+
+router.post('/login', async (req, res) => {
+    try {
+        // Task 1: Connect to `secondChance` in MongoDB through `connectToDatabase` in `db.js`.
+        const db = await connectToDatabase();
+        
+        console.log('[DEBUG] JWT_SECRET:', process.env.JWT_SECRET); // Add this in authRoutes.js
+
+        // Task 2: Access MongoDB `users` collection
+        const collection = await db.collection("users");
+        const count = await collection.countDocuments();
+        console.log(`DB collection.count: ${count}`);
+
+        // Task 3: Check for user credentials in database
+        let theUser = await collection.findOne({email: req.body.email });
+        if(!theUser) {
+            logger.info("No user is found with email");
+            return res.status(404).send("No user is found with email");
+        }
+
+        console.log(`theUser is found: ${req.body.email}`);
+
+        // Task 4: Check if the password matches the encrypted password and send appropriate message on mismatch
+        let result = await bcrypt.compare(req.body.password,theUser.password);
+        if(!result) {
+            logger.error('Passwords do not match');
+            return res.status(404).json({error: 'Wrong password'});
+        }
+                
+        
+        // Task 5: Fetch user details from a database
+        const userName = theUser.firstName;
+        const userEmail = theUser.email;
+        console.log(`user details ${userName}, ${userEmail}`); 
+
+        // Task 6: Create JWT authentication if passwords match with user._id as payload
+        const payload = { user: {id: theUser._id.toString(), } };
+        const authtoken = jwt.sign(payload, JWT_SECRET, {expiresIn: '1h'}); //token expires in 1 hour
+
+        console.log(`authtoken: ${authtoken}`); 
+        
+        res.json({authtoken, userName, userEmail });
+        // Task 7: Send appropriate message if the user is not found
+    } catch (e) {
+        logger.error(e.message);
+         return res.status(500).send('Internal server error');
+
+    }
+});
+
+
+
+
+
+
+
+
+
+
 module.exports = router; 
